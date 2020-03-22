a8bb904375f5566c001cf772fc59c6fe0bafbcf3
David Lima <david.lima50@etec.sp.gov.br>
2020-03-22 12:50:35 -0300

    sudo removed
    removed all sudo because theres no need

--- autoconf.sh
+++ autoconf.sh
@@ -5,132 +5,132 @@ cd ~/ ;
 # preparing
 echo '\nAdding apt repositories' ;
 
-sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner" ;
-sudo add-apt-repository -y ppa:webupd8team/haguichi ;
-sudo add-apt-repository ppa:inkscape.dev/stable -y ;
-sudo add-apt-repository ppa:lutris-team/lutris -y ;
+add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner" ;
+add-apt-repository -y ppa:webupd8team/haguichi ;
+add-apt-repository ppa:inkscape.dev/stable -y ;
+add-apt-repository ppa:lutris-team/lutris -y ;
 
 echo '\nconfig wine requirements' ;
 
-sudo dpkg --add-architecture i386 ;
+dpkg --add-architecture i386 ;
 wget -nc https://dl.winehq.org/wine-builds/winehq.key ;
-sudo apt-key add winehq.key ;
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ eoan main' ;
+apt-key add winehq.key ;
+apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ eoan main' ;
 
 echo '\nupdating apt' ;
 
-sudo apt update ;
+apt update ;
 
-sudo apt install unzip &&
+apt install unzip &&
 
 unzip bag.zip ;
 
 # install
 echo '\n\n\nterminator install' ;
 
-sudo apt install terminator ;
+apt install terminator ;
 
 echo '\n\n\nnet-tools install' ;
 
-sudo apt install net-tools ;
+apt install net-tools ;
 
 echo '\n\n\nhtop install' ;
 
-sudo apt install htop ;
+apt install htop ;
 
 echo '\n\n\node && npm && yarn install' ;
 
-sudo apt install nodejs -y && 
-sudo apt install npm -y &&
-sudo npm i -g npm@latest &&
-sudo npm i -g n &&
-sudo n stable &&
-sudo npm i -g yarn@latest ;
+apt install nodejs -y && 
+apt install npm -y &&
+npm i -g npm@latest &&
+npm i -g n &&
+n stable &&
+npm i -g yarn@latest ;
 
 echo '\n\n\ndocker install' ;
 
-sudo apt install docker.io -y;
-sudo systemctl start docker ;
-sudo systemctl enable docker ;
-sudo apt install docker-compose -y;
+apt install docker.io -y;
+systemctl start docker ;
+systemctl enable docker ;
+apt install docker-compose -y;
 
-sudo groupadd docker ;
-sudo usermode -aG docker $USER ;
+groupadd docker ;
+usermode -aG docker $USER ;
 
 echo '\n\n\nkazam install' ;
 
-sudo apt install kazam -y ;
+apt install kazam -y ;
 
 echo '\n\n\naudacity install' ;
 
-sudo apt install audacity -y ;
+apt install audacity -y ;
 
 echo '\n\n\ninkscape install' ;
 
-sudo apt install inkscape -y ;
+apt install inkscape -y ;
 
 echo '\n\n\ngimp install' ;
 
-sudo apt install gimp -y ;
+apt install gimp -y ;
 
 echo '\n\n\nkdenlive install' ;
 
-sudo apt install kdenlive -y ;
+apt install kdenlive -y ;
 
 echo '\n\n\nvirtualbox-qt install' ;
 
-sudo apt install virtualbox-qt -y ;
+apt install virtualbox-qt -y ;
 
 echo '\n\n\nsteam-installer install' ;
 
-sudo apt install steam-installer -y ;
+apt install steam-installer -y ;
 
 echo '\n\n\nbijiben install' ;
 
-sudo apt install bijiben -y ;
+apt install bijiben -y ;
 
 echo '\n\n\nobs-studio install' ;
 
-sudo apt install obs-studio -y ;
+apt install obs-studio -y ;
 
 echo '\n\n\nflash player install' ;
 
-sudo apt install adobe-flashplugin browser-plugin-freshplayer-pepperflash ;
+apt install adobe-flashplugin browser-plugin-freshplayer-pepperflash ;
 
 echo '\n\n\nexpo-cli install' ;
 
-sudo npm i -g expo-cli ;
+npm i -g expo-cli ;
 
 echo '\n\n\ncode install' ;
 
-sudo apt install ./dev/code.deb -y ;
+apt install ./dev/code.deb -y ;
 
 echo '\n\n\nchrome install' ;
 
-sudo apt install ./dev/chrome.deb -y ;
+apt install ./dev/chrome.deb -y ;
 
 echo '\n\n\nhamachi install' ;
 
-sudo apt install ./deb/hamachi -y && sudo apt install haguichi -y ;
+apt install ./deb/hamachi -y && apt install haguichi -y ;
 
 echo '\n\n\nminecraft install' ;
 
-sudo apt install ./dev/minecraft.deb -y ;
+apt install ./dev/minecraft.deb -y ;
 
 echo '\n\n\nwine install' ;
 
-sudo apt install -y --install-recommends winehq-stable ;
+apt install -y --install-recommends winehq-stable ;
 
 echo '\n\n\nlutris install' ;
 
-sudo apt install lutris -y;
+apt install lutris -y;
 
 echo '\n\n\nclipgrab install' ;
 
 chmod +x ./clipgrab/clipgrab.AppImage ;
-sudo cp -r ./clipgrab /opt/ ;
+cp -r ./clipgrab /opt/ ;
 
-sudo cp ./clipgrab/clipgrab.desktop ~/.local/share/applications/ ;
+cp ./clipgrab/clipgrab.desktop ~/.local/share/applications/ ;
 
 # extra config
 
@@ -143,19 +143,19 @@ gsettings set org.gnome.desktop.interface clock-show-seconds true
 ## theme
 echo '\n\n\nSetting theme' ;
 mkdir -p /usr/share/themes ;
-sudo cp -r ./appearance/themes/bluesky_dark /usr/share/themes/ ;
+cp -r ./appearance/themes/bluesky_dark /usr/share/themes/ ;
 gsettings set org.gnome.desktop.interface gtk-theme 'bluesky_dark' ;
 gsettings set org.gnome.desktop.wm.preferences theme 'bluesky_dark' ;
 
 ## icons
 echo '\n\n\nSetting icons' ;
-sudo mkdir -p /usr/share/icons ;
-sudo cp -r ./appearance/icons/blue_icons /usr/share/icons/ ;
+mkdir -p /usr/share/icons ;
+cp -r ./appearance/icons/blue_icons /usr/share/icons/ ;
 gsettings set org.gnome.desktop.interface icon-theme 'blue_icons' ;
 
 ## cursors
 echo '\n\n\nSetting cursors' ;
-sudo cp -r ./appearance/cursors/vimix_cursors /usr/share/icons/ ;
+cp -r ./appearance/cursors/vimix_cursors /usr/share/icons/ ;
 gsettings set  org.gnome.desktop.interface cursor-theme 'vimix_cursors' ;
 
 # add gnome shell plugins
@@ -185,17 +185,17 @@ git config --global alias.lg 'log --pretty="format:%Cgreen%cI %C(white)%cr%n -> 
 
 # install zsh
 echo '\n\n\ninstalling zsh'
-sudo apt install zsh -y;
+apt install zsh -y;
 chsh -s $(which zsh) ;
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ;
 
-# change file and executing pulseaudio -k && sudo alsa force-reload
+# change file and executing pulseaudio -k && alsa force-reload
 echo '\n\n\nnoise removing'
-sudo echo '### USER CHANGES' >> /etc/pulse/default.pa ;
-sudo echo '# echo cancel' >> /etc/pulse/default.pa ;
-sudo echo 'load-module module-echo-cancel' >> /etc/pulse/default.pa ;
-pulseaudio -k && sudo alsa force-reload ;
+echo '### USER CHANGES' >> /etc/pulse/default.pa ;
+echo '# echo cancel' >> /etc/pulse/default.pa ;
+echo 'load-module module-echo-cancel' >> /etc/pulse/default.pa ;
+pulseaudio -k && alsa force-reload ;
 
-echo 'run "pulseaudio -k && sudo alsa force-reload" to enable noise supress' ;
+echo 'run "pulseaudio -k && alsa force-reload" to enable noise supress' ;
 
 echo '\n\n\n***\n\n\n*finished*\n\n\n***\nmaybe you should restart'
